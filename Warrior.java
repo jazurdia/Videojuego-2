976e8c5b3411eb3d97560e24535a99edaf05c70b
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-10-16 15:03:06 -0600

    Create Warrior.java

---
+++ Warrior.java
@@ -0,0 +1,16 @@
+public class Warrior extends Hero{
+
+    public Warrior(String nombre, String mensaje, int vida, int attackPoints) {
+        super(nombre, mensaje, vida, attackPoints);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void addItemToBag(String name int effect) {
+        if(bagOfItems.size()<3){
+            bagOfItems.add
+        }
+        
+    }
+    
+}
