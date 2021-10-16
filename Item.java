b3d63234a21decfabd222ff7cfbdc00e9a139670
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-10-16 15:04:22 -0600

    Update Item.java
    method not working, erased.

--- Item.java
+++ Item.java
@@ -2,13 +2,11 @@ public class Item {
     String name;
     int effect; // daño causado.
 
-    Item(String name, int effect){
+    public Item(String name, int effect){
         this.name = name;
         this.effect = effect;
     }
 
-    public Item() {
-    }
 
     public String getName(){
         return name;
