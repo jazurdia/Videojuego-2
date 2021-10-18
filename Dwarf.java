cce02803ae5b5d922dde29c83ade82332d367535
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-10-17 21:36:31 -0600

    Create Dwarf.java

---
+++ Dwarf.java
@@ -0,0 +1,12 @@
+public class Dwarf extends Enemy{
+
+    public Dwarf(String name, String message) {
+        super(name, message);
+        //TODO Auto-generated constructor stub
+        this.vida = 15;
+        this.attackPoints = 8;
+        this.extradamage = 4;
+        this.nameOfAbility = "Martillo Poderoso";
+    }
+    
+}
