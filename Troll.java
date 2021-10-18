00a6897179bf04a325d5db33d8eba2562a308358
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-10-17 21:36:37 -0600

    Create Troll.java

---
+++ Troll.java
@@ -0,0 +1,15 @@
+public class Troll extends Enemy {
+
+    public Troll(String name, String message) {
+        super(name, message);
+        //TODO Auto-generated constructor stub
+        this.vida = 20;
+        this.attackPoints = 7;
+        this.extradamage = 13;
+        this.nameOfAbility = "Super Hachazo";
+
+    }
+
+    
+    
+}
