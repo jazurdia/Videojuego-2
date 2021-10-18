f04385cc3736b60140482c8577c63474625b1391
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2021-10-17 21:36:34 -0600

    Create Orc.java

---
+++ Orc.java
@@ -0,0 +1,13 @@
+public class Orc extends Enemy {
+
+    public Orc(String name, String message) {
+        super(name, message);
+        //TODO Auto-generated constructor stub
+
+        this.vida = 5;
+        this.attackPoints = 3;
+        this.extradamage = 18;
+        this.nameOfAbility = "Ultra Vergazo";
+    }
+    
+}
